4084426b4752b27569b01ca4ff0ae2fc21da03a0
halfaoui <halfaouianis532@gmail.com>
2026-03-08 16:38:30 +0100

    initial clean commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# [PPC] - TME8 (Scala)
+
+## How to run
+
+### 1. Run player
+```
+cd PLAYER
+sbt
+compile
+run
+```
+
+### 2. Run music
+```
+cd TME8
+sbt
+compile
+run
+```
+in order to rerun : exit (ctrl+c) and rerun.
---
+++ TME8/src/main/scala/bach.scala
@@ -0,0 +1,262 @@
+
+package upmc.akka.culto
+
+import math._
+
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
+import ExecutionContext.Implicits.global
+import akka.actor.{Props, Actor, ActorRef, ActorSystem}
+
+
+abstract class ObjectMusical
+abstract class SimpleObjectMusical () extends ObjectMusical
+abstract class ComposeObjectMusical () extends ObjectMusical
+case class Note (pitch:Int, dur:Int, vel:Int) extends SimpleObjectMusical
+case class Rest (dur:Int) extends SimpleObjectMusical
+case class Sequential (elements:List[ObjectMusical]) extends ComposeObjectMusical
+case class Parallel (elements:List[ObjectMusical]) extends ComposeObjectMusical
+
+case class MidiNote (pitch:Int, vel:Int, dur:Int, at:Int) 
+  
+/////////////////////////////////////////////////
+
+class BachActor extends Actor {
+
+//////////////////////////////////////////////////
+ val remote =
+  context.actorSelection("akka.tcp://Player@127.0.0.1:6000/user/Instrument")
+
+////////////////////////////////////////////////
+
+def receive = {
+    case "START" => {
+      // figure 1
+      play(figure_1)
+
+      // figure 2
+      play(figure_2)
+
+      // figure 3
+      play(figure_3)
+
+      // question 5 / figure (6) - Canon de Bac
+      play(figure_bach)
+    }
+}
+
+/////////////////////////////////////////////////
+
+//Question 1
+
+  // voix 1 : do - mi - sol - silence - re
+  val voix1_ex = Sequential(List(
+    Note(60, 1000, 100), Note(64, 1000, 100), Note(67, 1000, 100), Rest(500), Note(62, 500, 100)
+  ))
+
+  // voix 2 : re (en deux temps) - sol - fa
+  val voix2_ex = Sequential(List(
+    Note(62, 2000, 100), Note(67, 1000, 100), Note(65, 1000, 100)
+  ))
+
+  // Les deux voix en parallele
+  val figure_1 = Parallel(List(voix1_ex, voix2_ex))
+
+ // Calcule la duree d'un objet musical
+  def duration (obj:ObjectMusical):Int =
+  obj match {
+    case Note(p,d,v) => d
+    case Rest(d) => d
+    case Sequential (l) => (l.map(duration)).foldLeft(0)(_+_)
+    case Parallel (l) => (l.map(duration)).foldLeft(0)(math.max)  
+  }
+
+
+//Question 3
+  def play (obj:ObjectMusical): Unit = {
+    play_midi (obj, 0)
+  }
+
+  def send_a_note (p:Int, d:Int, v:Int, at:Int): Unit = {
+    remote ! MidiNote(p,v,d,at)
+  }
+
+  
+  def play_midi (obj:ObjectMusical, at:Int): Unit =
+  obj match {
+    case Note(p,d,v) => send_a_note (p,d,v,at)
+    case Rest(d) => Nil
+    case Sequential (l) => {var date = at
+                        l.foreach(n=>{play_midi(n,date); date = date + duration(n)})}
+    case Parallel (l) => l.foreach(n=>play_midi(n,at))
+  }
+  
+ // Copy un objet musical
+  def copy (obj:ObjectMusical):ObjectMusical =
+  obj match {
+    case Note(p,d,v) => Note(p,d,v)
+    case Rest(d) => Rest(d)
+    case Sequential (l) => Sequential (l.map(copy))
+    case Parallel (l) => Parallel (l.map(copy))
+  }
+
+  // Compte le nombre de notes d'un objet musical
+  def note_count (obj:ObjectMusical):Int =
+  obj match {
+    case Note(p,d,v) => 1
+    case Parallel (l) => (l.map(note_count)).foldLeft(0)(_+_)
+    case Sequential (l) => (l.map(note_count)).foldLeft(0)(_+_)
+    case _ => 0
+  }
+ 
+  // Strech un objet musical par un factor fact
+  def stretch (obj:ObjectMusical, fact:Double ):ObjectMusical =
+  obj match {
+    case Note(p,d,v) => Note(p,(d*fact).toInt,v)
+    case Rest(d) => Rest((d*fact).toInt)
+    case Parallel (l) => Parallel (l.map(stretch (_,fact)))
+    case Sequential (l) => Sequential (l.map(stretch (_,fact)))
+  }
+
+// Question 4 - Figure 2 : Identite ; Transposition (12 tons) ; Retrograde ; Miroir (centre = 60)
+
+  // HAUT : Do, Mi-Sol, Silence, La
+  val fig_top = Sequential(List(
+    Note(60, 500, 100), Note(64, 250, 100), Note(67, 250, 100), Rest(500), Note(69, 500, 100)
+  ))
+  // BAS : Do, Sol, Fa
+  val fig_bot = Sequential(List(
+    Note(48, 1000, 100), Note(55, 500, 100), Note(53, 500, 100)
+  ))
+  val figure_originale = Parallel(List(fig_top, fig_bot))
+
+  // Transposition 12 demitons
+  val figure_transposee = transpose(figure_originale, 12)
+
+  // Retrograde
+  val figure_retrograde = retrograde(figure_originale)
+
+  // Miroir centre = 60
+  val figure_miroir = mirror(figure_originale, 60)
+
+  // Les 4 transformations en sequence
+  val figure_2 = Sequential(List(
+    figure_originale,      // Identite
+    Rest(500),
+    figure_transposee,     // Transposition 12 tons
+    Rest(500),
+    figure_retrograde,     // Retrograde
+    Rest(500),
+    figure_miroir          // Miroircentre = 60
+  ))
+
+// Transpose obj de n demitons
+  def transpose (obj:ObjectMusical, n:Int ):ObjectMusical =
+obj match {
+  case Note(p,d,v) => Note(p+n,d,v)
+  case Rest(d) => Rest(d)
+  case Parallel (l) => Parallel (l.map(transpose (_,n)))
+  case Sequential (l) => Sequential (l.map(transpose (_,n)))
+}
+
+// mirror de obj au tour du center c 
+  def mirror (obj:ObjectMusical, c:Int ):ObjectMusical =
+obj match {
+  case Note(p,d,v) => Note(c - (p - c),d,v)
+  case Rest(d) => Rest(d)
+  case Parallel (l) => Parallel (l.map(mirror (_,c)))
+  case Sequential (l) => Sequential (l.map(mirror (_,c)))
+}
+
+// retrograde un obj  
+  def retrograde (obj:ObjectMusical):ObjectMusical =
+obj match {
+  case Sequential (l) => Sequential (l.reverse.map(retrograde))
+  case o => o
+}
+
+// Question 4 - Figure 3 : canon (repeat figure_1 3) 1000
+  val figure_3 = canon(repeat(figure_1, 3), 1000)
+
+
+//Question 5
+
+// make a sequential avec n fois obj  
+  def repeat (obj:ObjectMusical, n:Int):ObjectMusical =
+    Sequential(List.fill(n)(copy(obj)))
+
+// make obj en parallele avec lui meme avec un decalage de n ms.
+  def canon (obj:ObjectMusical, n:Int):ObjectMusical =
+    Parallel(List(obj, Sequential(List(Rest(n), copy(obj)))))
+
+//  Met obj1 et obj2 en sequence 
+  def concat (obj1:ObjectMusical, obj2:ObjectMusical):ObjectMusical =
+    Sequential(List(obj1, obj2))
+
+
+//Question 5 BACH
+ val voix1 = Sequential ( List (
+  Note (60 , 750 , 106 ) , Note (62 , 250 , 108 ) , Note (63 , 250 , 108 ) , 
+  Note (64 , 250 , 109 ) , Note (65 , 250 , 109 ) , Note (66 , 250 , 110 ) , 
+  Note (67 , 1000 , 110 ) , Note (68 , 625 , 113 ) ,Note (65 , 125 , 114 ) , 
+  Note (61 , 125 , 112 ) , Note (60 , 125 , 112 ) , Note (59 , 500 , 112 ) , 
+  Rest (500) , Rest (500) , Note (67 , 1000 , 109 ) ,Note (66 , 1000 , 108 ) , 
+  Note (65 , 1000 , 106 ) , Note (64 , 1000 , 106 ) , Note (63 , 1000 , 106 ) , 
+  Note (62 , 750 , 106 ) , Note (61 , 250 , 106 ) , Note (58 , 250 , 106 ) , 
+  Note (57 , 250 , 106 ) , Note (62 , 500 , 106 ) ,Rest (1000),
+  Note (67 , 1000 , 106 ) , Note (65 , 500 , 106 ) , Note (64 , 1000 , 106 )))
+
+val voix2 = Sequential (List (
+  Rest (125) , Note (48 , 125 , 100 ), Note (51 , 125 , 100 ),
+  Note (55 , 125 , 100 ),Note (60 , 1000 , 100 ),Note (58 , 250 , 100 ), 
+  Note (57 , 250 , 100 ),Note (58 , 625 , 100 ),Note (52 , 125 , 100 ),
+  Note (50 , 125 , 100 ),Note (52 , 125 , 100 ),Note (53 , 125 , 100 ), 
+  Note (48 , 125 , 100 ),Note (53 , 125 , 100 ),Note (55 , 125 , 100 ),
+  Note (56 , 750 , 100 ),Note (56 , 250 , 100 ),Note (55 , 250 , 100 ), 
+  Note (53 , 250 , 100 ),Note (51 , 625 , 100 ),Note (51 , 125 , 100 ), 
+  Note (53 , 125 , 100 ),Note (51 , 125 , 100 ),Note (50 , 250 , 100 ),
+  Note (48 , 250 , 100 ),Note (49 , 500 , 100 ), Rest (250) ,Note (50 , 250 , 100 ), 
+  Note (51 , 250 , 100 ),Note (50 , 250 , 100 ),Note (48 , 125 , 100 ),
+  Note (47 , 125 , 100 ),Note (48 , 125 , 100 ),Note (47 , 125 , 100 ), 
+  Note (48 , 125 , 100 ),Note (50 , 125 , 100 ),Note (48 , 125 , 100 ),
+  Note (46 , 125 , 100 ),Note (45 , 125 , 100 ),Note (43 , 125 , 100 ), 
+  Note (45 , 125 , 100 ),Note (46 , 125 , 100 ),Note (48 , 125 , 100 ),
+  Note (45 , 125 , 100 ),Note (46 , 125 , 100 ),Note (48 , 125 , 100 ), 
+  Note (50 , 250 , 100 ),Note (60 , 500 , 100 ),Note (58 , 125 , 100 ), 
+  Note (57 , 125 , 100 ),Note (58 , 250 , 100 ),Note (55 , 250 , 100 ),
+  Note (52 , 250 , 100 ),Note (57 , 125 , 100 ),Note (55 , 125 , 100 ), 
+  Note (54 , 250 , 100 ),Note (55 , 125 , 100 ),Note (57 , 125 , 100 ),
+  Note (58 , 250 , 100 ), Note (49 , 250 , 100 ),Note (50 , 500 , 100 ), 
+  Rest (500) , Rest (250) , Note (50 , 375 , 100 ),Note (53 , 125 , 100 ),
+  Note (52 , 125 , 100 ),
+  Note (50 , 125 , 100 ),Note (49 , 125 , 100 ),Note (50 , 125 , 100 ), 
+  Note (52 , 125 , 100 ),Note (53 , 125 , 100 ),Note (55 , 125 , 100 ),
+  Note (58 , 125 , 100 ),Note (57 , 125 , 100 ),Note (55 , 125 , 100 )))
+
+// Question 5 - Canon Bach (figure 6)
+
+  def canon_Bach ():ObjectMusical = {
+    // voix 1 : roi
+    val v1 = Sequential((0 to 5).map(i => transpose(voix1, i * 2)).toList)
+
+    // Voix 2 : canon
+    val v2 = Sequential((0 to 5).map(i => transpose(voix2, i * 2)).toList)
+
+    // Voix 3 : voix 2 transposee decalee(2000 ms)
+    val v3 = Sequential(Rest(2000) :: (0 to 5).map(i => transpose(voix2, 7 + i * 2)).toList)
+
+    // final
+    Parallel(List(v1, v2, v3))
+  }
+
+  val figure_bach = canon_Bach()
+}
+
+
+/**************** MAin object *******************/
+object bach extends App {
+  val system = ActorSystem("Bach")
+  val localActor = system.actorOf(Props[BachActor], name = "Bach")
+  localActor ! "START"
+}
+
